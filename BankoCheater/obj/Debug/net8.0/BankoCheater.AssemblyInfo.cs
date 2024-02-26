f4a1c2b1262f29f99e0915feeafeffb041818654
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2024-02-26 12:12:41 +0100

    Tried to commit

--- BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
+++ BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f729700476abf1f9ac638f78227ad6d1de37d76f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
